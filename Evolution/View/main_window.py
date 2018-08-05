1af8df27fd902ba427f92def911788f14d6a9f6e
ghosta0815 <Manuel.Gschrey@gmail.com>
2018-08-05 21:08:29 +0200

    Quitting the application added

--- Evolution/View/main_window.py
+++ Evolution/View/main_window.py
@@ -1,10 +1,22 @@
 from PyQt5.QtWidgets import QMainWindow
+from PyQt5 import QtCore
 from View.Ui_MainWindow import Ui_MainWindow
 
 class MainWindow(QMainWindow):
     """Class that represents the Main Window"""
     def __init__(self):
         super(MainWindow, self).__init__()
+
+        self._init_ui()
+
+    def _init_ui(self):
+        """Sets up the UI"""
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+
+        self.ui.quit_button.clicked.connect(self.quit_application)
         self.show()
+
+    def quit_application(self):
+        """Quits the application"""
+        QtCore.QCoreApplication.quit()
