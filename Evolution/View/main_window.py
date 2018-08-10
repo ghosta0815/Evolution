933c73198dd796406cbc45826ccaa0199ef76827
ghosta0815 <Manuel.Gschrey@gmail.com>
2018-08-10 16:51:40 +0200

    Added some stuff to make pylint work

--- Evolution/View/main_window.py
+++ Evolution/View/main_window.py
@@ -1,6 +1,9 @@
-from PyQt5.QtWidgets import QMainWindow
 from PyQt5 import QtCore
+from PyQt5 import QtGui
+from PyQt5.QtWidgets import QMainWindow
 from View.Ui_MainWindow import Ui_MainWindow
+import random
+
 
 class MainWindow(QMainWindow):
     """Class that represents the Main Window"""
@@ -12,10 +15,24 @@ class MainWindow(QMainWindow):
     def _init_ui(self):
         """Sets up the UI"""
         self.ui = Ui_MainWindow()
-        self.ui.setupUi(self)
+        self.ui.setupUi(self)        
 
         self.ui.quit_button.clicked.connect(self.quit_application)
         self.show()
+
+    def paintEvent(self, e):
+        qp = QtGui.QPainter()
+        qp.begin(self)
+        self.drawPoints(qp)
+        qp.end()
+
+    def drawPoints(self, qp):
+        qp.setPen(QtCore.Qt.red)
+        size = self.size()
+        for i in range(1000):
+            x = random.randint(1, size.width()-1)
+            y = random.randint(1, size.height()-1)
+            qp.drawPoint(x, y)
 
     def quit_application(self):
         """Quits the application"""
