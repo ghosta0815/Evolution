04b557f85c9a0355a1a7771ef759c66d83372c79
ghosta0815 <Manuel.Gschrey@gmail.com>
2018-08-15 20:48:27 +0200

    Works now also with obstacles

--- Evolution/Model/dot.py
+++ Evolution/Model/dot.py
@@ -13,9 +13,14 @@ class Direction(Enum):
 class Dot():
     """Point on an x-y plain, that moves in a certain pattern"""
 
-    def __init__(self, startpos_x, startpos_y, movement_speed):
-        self.fitness = 0
-        self.mutation_strength = 0.05
+    def __init__(self, startpos_x, startpos_y):
+        self.distance = 0
+        self.target_reached_at = -1
+        self.relative_fitness = 0
+        self.mutation_strength = 0.1
+
+        self.direction_change_propability = 0.5
+        self.movement_directions = []
 
         self.movement_counter = 0
         self.start_pos_x = startpos_x
@@ -23,7 +28,6 @@ class Dot():
         self.pos_x = startpos_x
         self.pos_y = startpos_y
         self.movement_speed = movement_speed
-        self.movement_directions = []
 
     def generate_first_generation(self, max_movement):
         """Returns directions for the first generation of moving the dot"""
@@ -38,14 +42,14 @@ class Dot():
             if random.random() < self.mutation_strength:
                 self.movement_directions[i] = random.choice(list(Direction))
 
-    def calculate_fitness(self, target):
+    def calculate_distance(self, target):
         """Calculates the fitness based on the distance to a target"""
-        self.fitness = 1/(math.sqrt((target.pos_x - self.pos_x)**2 \
-            + (target.pos_y - self.pos_y)**2) + 1)
+        self.distance = math.sqrt((target.pos_x - self.pos_x)**2 \
+            + (target.pos_y - self.pos_y)**2)
 
     def move(self, board):
         """Moves the dot on the board, based on the trained movement directions"""
-        if self.movement_counter < len(self.movement_directions):
+        if self.movement_counter < len(self.movement_directions) and self.target_reached_at == -1:
             movement_direction = self.movement_directions[self.movement_counter]
             self.movement_counter += self.movement_speed
             if movement_direction == Direction.UP:
@@ -64,9 +68,10 @@ class Dot():
                 self.pos_x -= self.movement_speed
                 if board.is_outside(self.pos_x, self.pos_y):
                     self.pos_x += self.movement_speed
-
+            if board.target_reached(self.pos_x, self.pos_y):
+                self.target_reached_at = self.movement_counter
 
 if __name__ == "__main__":
     # pylint: disable=W0212
-    DOT = Dot(0, 0, 2)
+    DOT = Dot(0, 0)
     print(DOT.generate_first_generation(20))
